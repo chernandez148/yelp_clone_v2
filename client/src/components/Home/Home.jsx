89800aac6166a176a0d74564348d8cf68c78da22
Christian <hernandezchristian94@gmail.com>
2023-04-26 16:12:58 -0700

    update

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import SlidingWords from '../SlidingWords/SlidingWords'
-import BusinessList from '../BusinessList/BusinessList'
+// import BusinessList from '../BusinessList/BusinessList'
 import { Button, Container } from 'react-bootstrap'
 import './Home.css'
 
 function Home({ businesses }) {
-    const renderBusinesses = businesses.map(businessData =>
-        <BusinessList
-            id={businessData.business_id}
-            name={businessData.business_name}
-            category={businessData.business_category}
-            image={businessData.business_image}
-            phone_numer={businessData.business_number}
-            address={businessData.business_address}
-            city={businessData.business_city}
-            state={businessData.business_state}
-            zipcode={businessData.business_zipcode}
-            description={businessData.business_description}
-        />)
+    // const renderBusinesses = businesses.map(businessData =>
+    //     <BusinessList
+    //         id={businessData.business_id}
+    //         name={businessData.business_name}
+    //         category={businessData.business_category}
+    //         image={businessData.business_image}
+    //         phone_numer={businessData.business_number}
+    //         address={businessData.business_address}
+    //         city={businessData.business_city}
+    //         state={businessData.business_state}
+    //         zipcode={businessData.business_zipcode}
+    //         description={businessData.business_description}
+    //     />)
     return (
         <div className='Home d-flex flex-column position-absolute w-100'>
             <Container className='d-flex justify-content-center align-items-center vh-100'>
@@ -27,7 +27,7 @@ function Home({ businesses }) {
             </Container>
             <div className='local-businesses py-5'>
                 <h2 className='text-center'>Local Businesses</h2>
-                {renderBusinesses}
+                {/* {renderBusinesses} */}
             </div>
         </div>
     )
